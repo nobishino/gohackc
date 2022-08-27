517dfbaebadc55a10f3fa814d9a4472cbfdc96ed
nobishino <nobishii11@gmail.com>
2022-08-27 16:22:52 +0900

    implement symbol

--- tokenizer/runner.go
+++ tokenizer/runner.go
@@ -3,13 +3,25 @@ package tokenizer
 import "io"
 
 func ToXML(dst io.Writer, t *Tokenizer) error {
+	if _, err := io.WriteString(dst, "<tokens>\n"); err != nil {
+		return err
+	}
+	defer io.WriteString(dst, "</tokens>")
 loop:
 	for t.HasMoreTokens() {
 		t.Advance()
 		switch t.currentToken.tokenType {
 		case EOF:
 			break loop
+		case SYMBOL:
+			if _, err := io.WriteString(dst, symbolTag(t.currentToken)); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
+}
+
+func symbolTag(tk token) string {
+	return "<symbol> " + tk.symbol + " </symbol>\n"
 }
