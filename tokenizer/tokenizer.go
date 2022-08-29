93edb62524244bdf69d1d9656b57c3d610cdda74
nobishino <nobishii11@gmail.com>
2022-08-29 18:52:03 +0900

    Process line/block comments

--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -35,9 +35,14 @@ func (t *Tokenizer) currentLetter() rune {
 // 次のトークンを取得し、それをカレントトークンとする.
 // HasMoreTokens()がtrueの場合のみ呼び出すことができる
 func (t *Tokenizer) Advance() {
+	if t.atEOF() {
+		t.hasMoreTokens = false
+		t.currentToken = token{tokenType: EOF}
+		return
+	}
 	// skip spaces, tabs, and line separators
-	t.skipDelimiters()
-	if t.pos >= len(t.sourceCode) {
+	t.skipDelimitersOrComments()
+	if t.atEOF() {
 		t.hasMoreTokens = false
 		t.currentToken = token{tokenType: EOF}
 		return
@@ -58,14 +63,86 @@ func (t *Tokenizer) Advance() {
 	panic("undefined")
 }
 
+func (t *Tokenizer) atEOF() bool {
+	return t.pos >= len(t.sourceCode)
+}
+
 func isSymbol(r rune) bool {
 	return strings.ContainsRune("{}()[].,;+-*/&|<>=~", r)
+}
+
+func (t *Tokenizer) skipDelimitersOrComments() {
+	for t.atCommentStart() || t.atDelimiters() {
+		t.skipDelimiters()
+		t.skipLineComment()
+		t.skipBlockComment()
+	}
 }
 
 func (t *Tokenizer) skipDelimiters() {
 	for t.pos < len(t.sourceCode) && isDelimiter(rune(t.sourceCode[t.pos])) {
 		t.pos++
 	}
+}
+
+func (t *Tokenizer) atDelimiters() bool {
+	return !t.atEOF() && isDelimiter(t.currentLetter())
+}
+
+func (t *Tokenizer) skipLineComment() {
+	if t.atCommentStart() {
+		t.skipUntilLF()
+	}
+}
+
+// いまいる位置にコメントの開始シーケンスが存在するかどうかを返す
+func (t *Tokenizer) atCommentStart() bool {
+	if t.atLineCommentStart() {
+		return true
+	}
+	return t.atBlockCommentStart()
+}
+
+func (t *Tokenizer) atLineCommentStart() bool {
+	if t.pos >= len(t.sourceCode)-1 { // 最後の1文字ならコメントの開始にはならない
+		return false
+	}
+	return t.sourceCode[t.pos:t.pos+2] == "//"
+}
+
+func (t *Tokenizer) skipBlockComment() {
+	if t.atBlockCommentStart() {
+		t.skipUntilBlockCommentEnd()
+	}
+}
+
+func (t *Tokenizer) atBlockCommentStart() bool {
+	if t.pos >= len(t.sourceCode)-2 { // 最後の2文字以降ならブロックコメントの開始にはならない
+		return false
+	}
+	return t.sourceCode[t.pos:t.pos+3] == "/**"
+}
+
+func (t *Tokenizer) atBlockCommentEnd() bool {
+	if t.pos < 2 { // 最低でも3文字目以降でなければならない
+		return false
+	}
+	return t.sourceCode[t.pos-2:t.pos] == "*/"
+}
+
+func (t *Tokenizer) skipUntilLF() {
+	for t.pos < len(t.sourceCode) && t.currentLetter() != '\n' {
+		t.pos++
+	}
+}
+
+func (t *Tokenizer) skipUntilBlockCommentEnd() {
+	for t.pos < len(t.sourceCode) {
+		if t.atBlockCommentEnd() {
+			return
+		}
+	}
+	panic("Block comment begins, but does not ends properly")
 }
 
 func isDelimiter(r rune) bool {
--- tokenizer/tokenizer_test.go
+++ tokenizer/tokenizer_test.go
@@ -16,7 +16,7 @@ func TestTokenizer_SmallSource(t *testing.T) {
 		{"", "<tokens>\n</tokens>"},
 		{"\r \t\n\n\t", "<tokens>\n</tokens>"},
 		{"{", "<tokens>\n<symbol> { </symbol>\n</tokens>"},
-		// {"{//}", "<tokens>\n<symbol> { </symbol>\n</tokens>"},
+		{"\r\n { /**hello*/\n\r \t//\t }\n", "<tokens>\n<symbol> { </symbol>\n</tokens>"},
 	}
 	for _, tc := range testcases {
 		t.Run("", func(t *testing.T) {
