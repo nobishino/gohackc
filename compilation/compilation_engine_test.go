f3588d72e43200c4c43936e5acd13adbc2001171
nobishino <nobishii11@gmail.com>
2022-09-16 08:54:00 +0900

    Enable ArrayTest case

--- compilation/compilation_engine_test.go
+++ compilation/compilation_engine_test.go
@@ -30,7 +30,7 @@ func TestCompilationEngine(t *testing.T) {
 		{"exp_less_square_main", OK},
 		{"exp_less_square_game", OK},
 		{"exp_less_square_square", OK},
-		// {"array_test", OK},
+		{"array_test", OK},
 		// {"square_main", OK},
 		// {"square_game", OK},
 		// {"square_square", OK},
